4f2f39a0bb7c05cbdacb423a9e837d8516ff2b03
Jon Jagger <jon@jaggersoft.com>
2018-12-22 15:11:50 +0000

    try curl trigger back in script

--- .circleci/trigger_dependent_projects.sh
+++ .circleci/trigger_dependent_projects.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-readonly API_TOKEN="${1}"
+readonly API_TOKEN=${1}
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
-. "${MY_DIR}/trigger_project.sh"
+. ${MY_DIR}/trigger_project.sh
 # images built FROM cyberdojo/rack-base
-trigger_project "${API_TOKEN}" differ
-trigger_project "${API_TOKEN}" saver
+trigger_project ${API_TOKEN} differ
+trigger_project ${API_TOKEN} saver
