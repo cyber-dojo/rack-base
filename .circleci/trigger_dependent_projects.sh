fc278c0bca403889f6ec909b0010debbc46f9b4e
Jon Jagger <jon@jaggersoft.com>
2018-12-22 15:14:27 +0000

    put curl triggers into main circleci script

--- .circleci/trigger_dependent_projects.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-readonly API_TOKEN=${1}
-readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
-. ${MY_DIR}/trigger_project.sh
-# images built FROM cyberdojo/rack-base
-trigger_project ${API_TOKEN} differ
-trigger_project ${API_TOKEN} saver
