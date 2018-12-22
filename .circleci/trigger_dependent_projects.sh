d81b123753c79b7c41371d95fe618c6f7e6cfd3e
Jon Jagger <jon@jaggersoft.com>
2018-12-22 15:04:10 +0000

    pass api-token to sh functions

--- .circleci/trigger_dependent_projects.sh
+++ .circleci/trigger_dependent_projects.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -e
 
-echo "CIRCLECI==:${CIRCLECI}:"
-
+readonly API_TOKEN="${1}"
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
-. ${MY_DIR}/trigger_project.sh
-# These projects create an image FROM cyberdojo/rack-base
-trigger_project differ
-trigger_project saver
+. "${MY_DIR}/trigger_project.sh"
+# images built FROM cyberdojo/rack-base
+trigger_project "${API_TOKEN}" differ
+trigger_project "${API_TOKEN}" saver
--- .circleci/trigger_project.sh
+++ .circleci/trigger_project.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e
 
-readonly project="${1}"
+readonly api_token="${1}"
+readonly project="${2}"
 
 trigger_project()
 {
   local vcs_type="github"
   local username="cyber-dojo"
   if [ "${CIRCLECI}" = "true" ]; then
-    curl -X POST https://circleci.com/api/v1.1/project/${vcs_type}/${username}/${project}/build?circle-token=${CIRCLECI_BUILD_TOKEN}
+    curl -X "POST https://circleci.com/api/v1.1/project/${vcs_type}/${username}/${project}/build?circle-token=${api_token}"
   fi
 }
