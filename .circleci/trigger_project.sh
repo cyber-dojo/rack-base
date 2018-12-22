879671453d017b840afc56dc170e8ac75f6f88a0
Jon Jagger <jon@jaggersoft.com>
2018-12-22 15:06:05 +0000

    fix quote in wrong place

--- .circleci/trigger_project.sh
+++ .circleci/trigger_project.sh
@@ -9,6 +9,6 @@ trigger_project()
   local vcs_type="github"
   local username="cyber-dojo"
   if [ "${CIRCLECI}" = "true" ]; then
-    curl -X "POST https://circleci.com/api/v1.1/project/${vcs_type}/${username}/${project}/build?circle-token=${api_token}"
+    curl -X POST "https://circleci.com/api/v1.1/project/${vcs_type}/${username}/${project}/build?circle-token=${api_token}"
   fi
 }
