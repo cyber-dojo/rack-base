80a473bc90e1a1f9111f8c04ea6b0fba3209000a
Jon Jagger <jon@jaggersoft.com>
2019-02-19 20:38:33 +0000

    trigger differ and saver from .circleci/config.yml

---
+++ sh/trigger_dependent_repos.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ev
+
+declare -r SHA="${1}"
+declare -r REPO_URLS="${@:2}"
+
+for REPO_URL in ${REPO_URLS}; do             # eg https://github.com/cyber-dojo/differ.git
+  local -r REPO_NAME=$(basename "${1}" .git) # eg differ
+  cd /tmp
+  git clone "${REPO_URL}"
+  cd "${REPO_NAME}"
+  echo "${SHA}" > rack-base.trigger
+  git add .
+  git config --global user.email "cyber-dojo-machine-user@cyber-dojo.org"
+  git config --global user.name "Machine User"
+  git commit -m "automated build trigger from cyberdojo/rack-base ${SHA}"
+  git push origin master
+done
