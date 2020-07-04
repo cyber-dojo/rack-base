ff73f082d3b654570e4fe007bd0bdbca8e3b9bf6
Jon Jagger <jon@jaggersoft.com>
2020-07-04 13:57:15 +0100

    Delete dead repo triggering code in build script

--- .circleci/ruby-base.trigger
+++
@@ -1,1 +0,0 @@
-Automated build trigger from https://github.com/cyber-dojo/ruby-base/commit/997e10bd6191ad60dcc8370a63b0070f98646357
--- build_test_tag_publish.sh
+++ build_test_tag_publish.sh
@@ -94,51 +94,7 @@ assert_equal()
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
-on_ci_trigger_dependent_images()
-{
-  if ! on_ci; then
-    echo 'not on CI so not triggering dependent images'
-    return
-  fi
-  echo 'on CI so triggering dependent images'
-  curl_trigger_script
-  local -r from_org=cyber-dojo
-  local -r from_repo="$(my_name)"
-  local -r from_sha="$(git_commit_sha)"
-  local -r to_org=cyber-dojo
-  local -r to_repos="differ saver sinatra-base start-points-base "
-  $(trigger_script_path) \
-    "${from_org}" "${from_repo}" "${from_sha}" \
-    "${to_org}" "${to_repos}"
-}
-
-# - - - - - - - - - - - - - - - - - - - - - - - -
-curl_trigger_script()
-{
-  local -r raw_github_org=https://raw.githubusercontent.com/cyber-dojo
-  local -r repo=cyber-dojo
-  local -r branch=master
-  local -r path=sh/circle-ci
-  local -r url="${raw_github_org}/${repo}/${branch}/${path}/$(trigger_script_name)"
-  curl -O --silent --fail "${url}"
-  chmod 700 "$(trigger_script_path)"
-}
-
-# - - - - - - - - - - - - - - - - - - - - - - - -
-trigger_script_path()
-{
-  echo "./$(trigger_script_name)"
-}
-
-# - - - - - - - - - - - - - - - - - - - - - - - -
-trigger_script_name()
-{
-  echo github_automated_commit_push.sh
-}
-
-# - - - - - - - - - - - - - - - - - - - - - - - -
 build_image
 tag_image
 assert_equal SHA "$(git_commit_sha)" "$(image_sha)"
 on_ci_publish_tagged_image
-#on_ci_trigger_dependent_images
