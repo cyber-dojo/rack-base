426135ea74b0c24e2a88cc9c0ea031c8d457840c
Jon Jagger <jon@jaggersoft.com>
2023-01-07 09:26:44 +0000

    Switch to github workflow and add kosli reporting

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
-
-[![CircleCI](https://circleci.com/gh/cyber-dojo/rack-base.svg?style=svg)](https://circleci.com/gh/cyber-dojo/rack-base)
+[![Github Action (main)](https://github.com/cyber-dojo/rack-base/actions/workflows/main.yml/badge.svg)](https://github.com/cyber-dojo/rack-base/actions)
 
 # cyberdojo/rack-base docker image
 
 - A base docker image for [cyber-dojo](http://cyber-dojo.org) rack-based services.
-- Helps speed up docker builds.
 
 ![cyber-dojo.org home page](https://github.com/cyber-dojo/cyber-dojo/blob/master/shared/home_page_snapshot.png)
--- build_test_publish.sh
+++ build_test_publish.sh
@@ -1,97 +1,15 @@
-#!/bin/bash -Eeu
+#!/usr/bin/env bash
+set -Eeu
 
-readonly ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+export REPO_ROOT="$(git rev-parse --show-toplevel)"
+export SH_DIR="${REPO_ROOT}/sh"
+source "${SH_DIR}/kosli.sh"
+source "${SH_DIR}/lib.sh"
 
-# - - - - - - - - - - - - - - - - - - - - - - - -
-build_image()
-{
-  docker build \
-    --build-arg COMMIT_SHA="$(git_commit_sha)" \
-    --tag "$(image_name)" \
-    "${ROOT_DIR}/app"
-}
-
-# - - - - - - - - - - - - - - - - - - - - - - - -
-git_commit_sha()
-{
-  echo $(cd "${ROOT_DIR}" && git rev-parse HEAD)
-}
-
-# - - - - - - - - - - - - - - - - - - - - - - - -
-my_name()
-{
-  echo rack-base
-}
-
-# - - - - - - - - - - - - - - - - - - - - - - - -
-image_name()
-{
-  echo "cyberdojo/$(my_name)"
-}
-
-# - - - - - - - - - - - - - - - - - - - - - - - -
-image_sha()
-{
-  docker run --rm $(image_name):latest sh -c 'echo ${SHA}'
-}
-
-# - - - - - - - - - - - - - - - - - - - - - - - -
-image_tag()
-{
-  local -r sha="$(git_commit_sha)"
-  echo "${sha:0:7}"
-}
-
-# - - - - - - - - - - - - - - - - - - - - - - - -
-tag_image()
-{
-  local -r image="$(image_name)"
-  local -r tag="$(image_tag)"
-  docker tag "${image}:latest" "${image}:${tag}"
-  echo "$(git_commit_sha)"
-  echo "${tag}"
-}
-
-# - - - - - - - - - - - - - - - - - - - - - - - -
-on_ci_publish_tagged_image()
-{
-  if ! on_ci; then
-    echo 'not on CI so not publishing tagged image'
-    return
-  fi
-  echo 'on CI so publishing tagged image'
-  local -r image="$(image_name)"
-  local -r tag="$(image_tag)"
-  docker push ${image}:latest
-  docker push ${image}:${tag}
-}
-
-# - - - - - - - - - - - - - - - - - - - - - - - -
-on_ci()
-{
-  set +u
-  [ -n "${CIRCLECI}" ]
-  local -r result=$?
-  set -u
-  [ "${result}" == '0' ]
-}
-
-# - - - - - - - - - - - - - - - - - - - - - - - -
-assert_equal()
-{
-  local -r name="${1}"
-  local -r expected="${2}"
-  local -r actual="${3}"
-  echo "expected: ${name}='${expected}'"
-  echo "  actual: ${name}='${actual}'"
-  if [ "${expected}" != "${actual}" ]; then
-    echo "ERROR: unexpected ${name} inside image ${IMAGE}:latest"
-    exit 42
-  fi
-}
-
-# - - - - - - - - - - - - - - - - - - - - - - - -
-build_image
-tag_image
+build_docker_image
 assert_equal SHA "$(git_commit_sha)" "$(image_sha)"
+tag_image
 on_ci_publish_tagged_image
+on_ci_kosli_declare_pipeline
+on_ci_kosli_report_artifact
+#on_ci_kosli_report_synk_evidence
