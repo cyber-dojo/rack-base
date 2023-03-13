fa55dfecf81286e4f702d0eefdd183baef5a7b61
Jon Jagger <jon@jaggersoft.com>
2023-03-13 14:05:25 +0000

    Prepare for new Koslo CLI

--- sh/kosli.sh
+++ sh/kosli.sh
@@ -3,20 +3,20 @@ set -Eeu
 
 # KOSLI_OWNER is set in CI
 # KOSLI_API_TOKEN is set in CI
-# KOSLI_PIPELINE is set in CI
+# KOSLI_FLOW is set in CI
 # KOSLI_HOST_STAGING is set in CI
 # KOSLI_HOST_PRODUCTION is set in CI
 
 # - - - - - - - - - - - - - - - - - - -
-kosli_declare_pipeline()
+kosli_create_flow()
 {
   local -r hostname="${1}"
 
-  kosli pipeline declare \
+  kosli create flow "${KOSLI_FLOW}" \
     --description "Rack base image" \
-    --visibility public \
+    --host "${hostname}" \
     --template artifact,snyk-scan \
-    --host "${hostname}"
+    --visibility public
 }
 
 # - - - - - - - - - - - - - - - - - - -
@@ -24,11 +24,10 @@ kosli_report_artifact_creation()
 {
   local -r hostname="${1}"
 
-  kosli pipeline artifact report creation \
-    "$(artifact_name)" \
+  kosli report artifact "$(artifact_name)" \
       --artifact-type docker \
-      --repo-root "${REPO_ROOT}" \
-      --host "${hostname}"
+      --host "${hostname}" \
+      --repo-root "${REPO_ROOT}"
 }
 
 # - - - - - - - - - - - - - - - - - - -
@@ -36,12 +35,11 @@ kosli_report_snyk_evidence()
 {
   local -r hostname="${1}"
 
-  kosli pipeline artifact report evidence snyk \
-    "$(artifact_name)" \
+  kosli report evidence artifact snyk "$(artifact_name)" \
       --artifact-type docker \
+      --host "${hostname}" \
       --name snyk-scan \
-      --scan-results snyk.json \
-      --host "${hostname}"
+      --scan-results snyk.json
 }
 
 # - - - - - - - - - - - - - - - - - - -
@@ -49,8 +47,7 @@ kosli_assert_artifact()
 {
   local -r hostname="${1}"
 
-  kosli assert artifact \
-    "$(artifact_name)" \
+  kosli assert artifact "$(artifact_name)" \
       --artifact-type docker \
       --host "${hostname}"
 }
@@ -62,20 +59,18 @@ artifact_name()
 }
 
 # - - - - - - - - - - - - - - - - - - -
-on_ci_kosli_declare_pipeline()
+on_ci_kosli_create_flow()
 {
-  if on_ci
-  then
-    kosli_declare_pipeline "${KOSLI_HOST_STAGING}"
-    kosli_declare_pipeline "${KOSLI_HOST_PRODUCTION}"
+  if on_ci; then
+    kosli_create_flow "${KOSLI_HOST_STAGING}"
+    kosli_create_flow "${KOSLI_HOST_PRODUCTION}"
   fi
 }
 
 # - - - - - - - - - - - - - - - - - - -
 on_ci_kosli_report_artifact()
 {
-  if on_ci
-  then
+  if on_ci; then
     kosli_report_artifact_creation "${KOSLI_HOST_STAGING}"
     kosli_report_artifact_creation "${KOSLI_HOST_PRODUCTION}"
   fi
@@ -84,8 +79,7 @@ on_ci_kosli_report_artifact()
 # - - - - - - - - - - - - - - - - - - -
 on_ci_kosli_report_snyk_evidence()
 {
-  if on_ci
-  then
+  if on_ci; then
     snyk container test "$(artifact_name)" \
       --file="${REPO_ROOT}/app/Dockerfile" \
       --json-file-output=snyk.json \
@@ -99,8 +93,7 @@ on_ci_kosli_report_snyk_evidence()
 # - - - - - - - - - - - - - - - - - - -
 on_ci_kosli_assert_artifact()
 {
-  if on_ci
-  then
+  if on_ci; then
     kosli_assert_artifact "${KOSLI_HOST_STAGING}"
     kosli_assert_artifact "${KOSLI_HOST_PRODUCTION}"
   fi
